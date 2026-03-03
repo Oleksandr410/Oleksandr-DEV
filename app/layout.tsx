af36b1bf5444c29e1926a696de05d30e7752c1b2
phoenixfusionx <p.fusionx@outlook.com>
2026-03-03 14:52:03 -0800

    fix: resolve a hydrationwaring

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth" className={inter.variable}>
       <body className="antialiased">{children}</body>
     </html>
   );
